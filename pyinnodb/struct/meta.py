b43a38c36b57942d7bd204400aaeeacb714356b1
yongcai <yongcai@tencent.com>
2023-11-29 17:30:04 +0800

    add

---
+++ pyinnodb/struct/meta.py
@@ -0,0 +1,6 @@
+from elftools import construct
+from elftools.construct import Construct
+
+import logging
+
+logger = logging.getLogger(__name__)
--- pyinnodb/struct/metaclass.py
+++ pyinnodb/struct/metaclass.py
@@ -36,6 +36,9 @@ class CommonField:
         self.parser = parser
         self.is_bit = is_bit
 
+    def __call__(self, name):
+        return self.parser
+
 class MetaClass(type):
     def __new__(cls, name, bases, attrs):
         klass = super().__new__(cls, name, bases, attrs)
@@ -43,47 +46,27 @@ class MetaClass(type):
         for key, value in attrs.items():
             if isinstance(value, Field):
                 value.name = key
-                value.parser = value.parser(value.name) ## construct.UBIntxx(name)
+                # value.parser = value.parser(value.name) ## construct.UBIntxx(name)
                 fields.insert(bisect(fields, value), value)
         klass._parse_order = []
         klass._key_order = []
         bits_fields = []
         for f in fields:
             klass._key_order.append(f.name)
-            if isinstance(f.parser, MBits):
+            if isinstance(f.parser(f.name), MBits):
                 bits_fields.append(f)
                 continue
             elif len(bits_fields) > 0:
-                bits_parser = construct.EmbeddedBitStruct(*[f.parser._make_con() for f in bits_fields])
+                bits_parser = construct.EmbeddedBitStruct(*[f.parser(f.name)._make_con() for f in bits_fields])
                 bits_names = ",".join([f.name for f in bits_fields])
-                obj = CommonField(name=bits_names, parser=bits_parser, is_bit=True)
+                obj = Field(CommonField(name=bits_names, parser=bits_parser, is_bit=True))
+                obj.name = bits_names
                 bits_fields.clear()
                 klass._parse_order.append(obj)
             klass._parse_order.append(f)
         # klass._parse_order = fields
         klass.name = name
 
-        def _parse(self, stream, context=None):
-            start = stream.seek(0, 1)
-            for field in self._parse_order:
-                value = field.parser.parse_stream(stream)
-                if getattr(field, "is_bit", None):
-                    for key in field.name.split(","):
-                        setattr(self, key, getattr(value, key))
-                setattr(self, field.name, value)
-            end = stream.seek(0, 1)
-            self._consume_num = end - start
-
-            self_parse = attrs.get("_parse", None)
-            if self_parse is not None:
-                self_parse(self, stream, context)
-
-            return self
-
-        def _build(self, obj, stream, context=None):
-            for field in self._parse_order:
-                field.parser.build_stream(getattr(obj, field.name), stream)
-            
         def __str__(self):
             data = []
             for fname in self._key_order:
@@ -109,8 +92,8 @@ class MetaClass(type):
         klass.__init__ = __init__
         klass.__str__ = __str__
         klass.__repr__ = __str__
-        klass._parse = _parse
-        klass._build = _build
+        # klass._parse = _parse
+        # klass._build = _build
         #klass.parse_stream = _parse
         return klass
 
@@ -119,6 +102,32 @@ class Struct(Construct, metaclass=MetaClass):
         if obj is None:
             obj = self
         return super().build(obj)
+
+    def _parse(self, stream, context=None):
+        start = stream.seek(0, 1)
+        for field in self._parse_order:
+            try:
+                value = field.parser(field.name).parse_stream(stream)
+                if getattr(field.parser, "is_bit", None):
+                    for key in field.name.split(","):
+                            setattr(self, key, getattr(value, key))
+                setattr(self, field.name, value)
+            except Exception as e:
+                logger.error("self %s", self)
+                logger.error("parser is %s", field.name)
+                raise e
+        end = stream.seek(0, 1)
+        self._consume_num = end - start
+
+        # self_parse = attrs.get("_parse", None)
+        # if self_parse is not None:
+        #     self_parse(self, stream, context)
+
+        return self
+
+    def _build(self, obj, stream, context=None):
+        for field in self._parse_order:
+            field.parser(field.name).build_stream(getattr(obj, field.name), stream)
 
 class ArrayEntry(Construct):
     def __init__(self, subcon):
