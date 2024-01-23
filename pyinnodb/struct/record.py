2f6374cd866afeeef4a132af1e64b9d267ccebe1
Win Chua <winchua@foxmail.com>
2024-01-23 13:42:59 +0800

    delete OStruct

--- pyinnodb/struct/record.py
+++ pyinnodb/struct/record.py
@@ -1,6 +1,3 @@
-from elftools import construct
-from .meta import *
-
 from ..mconstruct import *
 
 
@@ -11,32 +8,3 @@ class MRecordHeader(CC):
     order: int = cfield(cs.BitsInteger(13))
     record_type: int = cfield(cs.BitsInteger(3))
     next_record_offset: int = cfield(cs.Int16sb)
-
-
-class RecordLeaf(OStruct):
-    pass
-    # Cluster Key field(k)
-    # Transaction ID(6)
-    # Roll Pointer(7)
-    # Non-key fields(j)
-
-
-class RecordNonLeaf(OStruct):
-    pass
-    # Cluster key min. on child page(k)
-    # child page num (4)
-
-
-class SecondaryIndex(OStruct):
-    pass
-    # Secondary Key (j)
-    # Clusterkey (k)
-
-# class ORecordHeader(Struct):  ## minimum of 5 bytes, there're other dynamic field
-#     info_flags = Field(Bits(4))
-#     record_owned_num = Field(Bits(4))
-#     order = Field(Bits(13))  # 0: infimum, 1: supremum, 2: user records
-#     record_type = Field(
-#         Bits(3)
-#     )  # 0: conventional; 1: node pointer; 2: infinum; 3: supremum
-#     next_record_offset = Field(construct.UBInt16)
