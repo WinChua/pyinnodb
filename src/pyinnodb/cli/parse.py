e817ec076fd4191054bbb8b9b611fc8ae7064517
yongcai <yongcai@tencent.com>
2024-05-12 23:13:22 +0800

    fix

--- src/pyinnodb/cli/parse.py
+++ src/pyinnodb/cli/parse.py
@@ -7,14 +7,18 @@ from pyinnodb import disk_struct
 @click.option("--type", multiple=True, type=click.STRING)
 @click.option("--offset", type=click.INT, default=0)
 @click.option("--remain", type=click.INT, default=0)
-def parse(ctx, pageno, type, offset, remain):
+@click.option("--func", type=click.STRING, default="")
+def parse(ctx, pageno, type, offset, remain, func):
     f = ctx.obj["fn"]
     f.seek(pageno * const.PAGE_SIZE)
     if offset != 0:
         f.seek(offset, 1)
     possible_names = [i for i in dir(disk_struct) if i.startswith("M")]
     for t in type:
-        print(t, getattr(disk_struct, t).parse_stream(f))
+        obj = getattr(disk_struct, t).parse_stream(f)
+        print(t, obj)
+        if getattr(obj, func, None) is not None:
+            print(func, getattr(obj, func, None)())
 
     print("byte consume is", f.tell() - pageno * const.PAGE_SIZE)
     if remain != 0:
