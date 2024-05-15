a9deb43e23637b7c299d015c0ba83413f139c391
WinChua <winchua@foxmail.com>
2024-05-15 11:16:21 +0800

    调整metaparse

--- pyinnodb/struct/__init__.py
+++ pyinnodb/struct/__init__.py
@@ -2,7 +2,7 @@ from elftools import construct
 from elftools.common.utils import struct_parse
 from .fil import *
 from .list import *
-from .xdes import *
 from .fsp import *
 from .inode import *
+from .xdes import *
 from .index import *
--- tests/test_xdes.py
+++ tests/test_xdes.py
@@ -2,21 +2,26 @@ from context import *
 
 from pyinnodb.struct import XdesEntry, ListNode, Pointer, BitArray
 
-def test_xdesc():
-    xe = XdesEntry(fseg_id = 1,
-              xdes_list = ListNode(
-                  prev = Pointer(page_number = 1, page_offset = 2),
-                  next = Pointer(page_number = 3, page_offset = 4),
-              ),
-              state = 5,
-              page_state = [0,1,0,1] * 16)
+
+def test_xdes():
+    xe = XdesEntry(
+        fseg_id=1,
+        xdes_list=ListNode(
+            prev=Pointer(page_number=1, page_offset=2),
+            next=Pointer(page_number=3, page_offset=4),
+        ),
+        state=5,
+        page_state=[0, 1, 0, 1] * 16,
+    )
     logger.info(str(xe))
     logger.info(xe.build())
     logger.info(len(xe.build()))
+    logger.info(xe._parse_order)
     assert len(xe.build()) == 40
+
 
 def test_bitarray():
     ba = BitArray(4, 2)
     for i in range(8):
-        data = ba("ba").build([0,0,0,i])
+        data = ba.build([0, 0, 0, i])
         logger.info("%d, %s, %d", i, data, len(data))
