46d10ddcb8ca239e0a03421fe9e93502d59b4c47
WinChua <winchua@foxmail.com>
2024-05-15 11:19:06 +0800

    feat: parse undo history

--- src/pyinnodb/cli/undo.py
+++ src/pyinnodb/cli/undo.py
@@ -3,6 +3,7 @@ from . import *
 from pyinnodb.disk_struct.undo_log import *
 from pyinnodb.disk_struct.fil import MFil
 from pyinnodb.disk_struct.trx import *
+from pyinnodb.disk_struct.rollback import MRollbackPointer
 
 @main.command()
 @click.pass_context
@@ -11,17 +12,52 @@ def undo_list(ctx, pageno):
     f = ctx.obj["fn"]
     fsp_page = ctx.obj["fsp_page"]
     f.seek(pageno * const.PAGE_SIZE)
-    # undo_page = MUndoPage.parse_stream(f)
-    # print(undo_page)
-    # print(undo_page.sizeof())
-    rseg_page = MTrxSysPage.parse_stream(f)
-    print(rseg_page)
+    undo_page = MUndoPage.parse_stream(f)
+    print(undo_page)
+    print(undo_page.sizeof())
 
 @main.command()
 @click.pass_context
 @click.option("--pageno", type=click.INT)
 @click.option("--offset", type=click.INT)
-def undo_record(ctx, pageno, offset):
+@click.option("--insert", type=click.INT)
+@click.option("--rsegid", type=click.INT)
+def undo_record(ctx, pageno, offset, insert, rsegid):
     f = ctx.obj['fn']
-    f.seek(pageno * const.PAGE_SIZE + offset)
-    print(MUndoRecordInsert.parse_stream(f))
+    undo_001 = open("datadir/undo_001", "rb")
+    undo_002 = open("datadir/undo_002", "rb")
+    undo_map = {1: undo_001, 2: undo_002}
+    rptr = MRollbackPointer(insert_flag=insert,
+            rollback_seg_id=rsegid,
+            page_number=pageno,
+            page_offset=offset)
+    while rptr is not None:
+        tmp = rptr
+        undo_record_header, rptr = rptr.last_version(undo_map)
+        print(tmp, undo_record_header)
+    # f.seek(pageno * const.PAGE_SIZE + offset)
+    # rollptr = MUndoRecordInsert.parse_stream(f)
+    # print(rollptr)
+    # # trx_undo_update_rec_get_sys_cols
+    # if updtype == 0: # update
+    #     info_bits = f.read(1)
+    #     trx_id = const.mach_u64_read_next_compressed(f)
+    #     ptr = const.mach_u64_read_next_compressed(f).to_bytes(7)
+    #     print(info_bits, trx_id, MRollbackPointer.parse(ptr))
+    # else: # insert
+    #     pass
+
+@main.command()
+@click.pass_context
+@click.option('--pageno', type=click.INT)
+def rseg_array(ctx, pageno):
+    f = ctx.obj['fn']
+    f.seek(pageno * const.PAGE_SIZE)
+    page = MRSEGArrayPage.parse_stream(f)
+    print(page)
+    for pageno in page.header.pagenos:
+        f.seek(pageno * const.PAGE_SIZE)
+        rseg_page = MRSEGPage.parse_stream(f)
+        pages = [f for f in rseg_page.slots if f != 4294967295]
+        if 150 in pages:
+            print(rseg_page, pageno)
--- src/pyinnodb/disk_struct/rollback.py
+++ src/pyinnodb/disk_struct/rollback.py
@@ -1,4 +1,6 @@
 from ..mconstruct import *
+from .. import const
+from .undo_log import MUndoRecordInsert
 
 
 class MRollbackPointer(CC):
@@ -6,3 +8,17 @@ class MRollbackPointer(CC):
     rollback_seg_id: int = cfield(cs.BitsInteger(7))
     page_number: int = cfield(cs.Int32ub)
     page_offset: int = cfield(cs.Int16ub)
+
+    def last_version(self, undo_map):
+        f = undo_map[self.rollback_seg_id]
+        f.seek(self.page_number * const.PAGE_SIZE + self.page_offset)
+        undo_record_header = MUndoRecordInsert.parse_stream(f)
+        if self.insert_flag == 0:
+            info_bits = f.read(1)
+            trx_id = const.mach_u64_read_next_compressed(f)
+            ptr = const.mach_u64_read_next_compressed(f)
+            ptr = MRollbackPointer.parse(ptr.to_bytes(7))
+            return undo_record_header, ptr
+        else:
+            return undo_record_header, None
+        
