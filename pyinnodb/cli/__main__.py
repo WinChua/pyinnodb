ef4fb090eb353291d7e4057f5544867a90a9b6e6
WinChua <winchua@foxmail.com>
2024-05-15 11:17:12 +0800

    remove pyelftools dep

--- pyinnodb/cli/__main__.py
+++ pyinnodb/cli/__main__.py
@@ -2,18 +2,22 @@ import click
 from . import static_usage
 from . import sql
 from pyinnodb.disk_struct.fsp import MFspPage
+import logging
 
 
 @click.group()
-@click.option("--fn", type=click.File("rb"))
+@click.argument("fn", type=click.File("rb"))
+@click.option(
+    "--log-level", type=click.Choice(["DEBUG", "ERROR", "INFO"]), default="ERROR"
+)
 @click.pass_context
-def main(ctx, fn):
+def main(ctx, fn, log_level):
+    logging.basicConfig(level=log_level)
     ctx.ensure_object(dict)
     ctx.obj["fn"] = fn
     fsp_page = MFspPage.parse_stream(fn)
     ctx.obj["fsp_page"] = fsp_page
-    print(
-        f"MySQL version {fsp_page.fil.pre_page}")
+    print(f"MySQL version {fsp_page.fil.pre_page}")
 
 
 main.command()(static_usage.static_page_usage)
