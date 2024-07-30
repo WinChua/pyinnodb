847031cba75dd7518d22ddbb69a1e543a3f1a67c
WinChua <winchua@foxmail.com>
2024-07-30 11:48:20 +0800

    test: refa test download file

--- tests/context.py
+++ tests/context.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import declarative_base
 from pyinnodb import const
 import sqlalchemy
 import pathlib
+import pytest
+from collections import namedtuple
 
 cur_file = pathlib.Path(__file__)
 
@@ -26,5 +28,30 @@ logger.setLevel(logging.DEBUG)
 
 Base = declarative_base()
 
+MysqlFile = namedtuple("MysqlFile", "mysql8ibd mysql5ibd mysql5frm")
+
+def download_test_file():
+    import requests
+    import tarfile
+    testfile = "https://github.com/user-attachments/files/16420657/test_data.tgz"
+    resp = requests.get(testfile)
+    fileobj = BytesIO(resp.content)
+    tar = tarfile.open(fileobj=fileobj)
+    tar.extractall(test_mysql8_ibd.parent.parent)
+
+@pytest.fixture
+def mysqlfile():
+    if not test_mysql8_ibd.exists():
+        print("download test file")
+        download_test_file()
+
+    with open(test_mysql8_ibd, "rb") as f8:
+        with open(test_mysql5_frm, "rb") as f5:
+            with open(test_mysql5_frm, "rb") as ff5:
+                yield MysqlFile(
+                    mysql8ibd=f8,
+                    mysql5ibd=f5,
+                    mysql5frm=ff5,
+                )
 
 
